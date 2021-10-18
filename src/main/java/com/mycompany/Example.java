5f045459179e2b03602d675c98d44c6797c02684
agaramudhala <54115786+agaramudhala@users.noreply.github.com>
2021-10-18 15:49:28 +0530

    Update Example.java

--- src/main/java/com/mycompany/Example.java
+++ src/main/java/com/mycompany/Example.java
@@ -6,7 +6,7 @@ public class Example {
     public static void main(String[] args) {
         try (Playwright playwright = Playwright.create()) {
             //Browser browser = playwright.chromium().launch();
-            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true).setSlowMo(50));
             Page page = browser.newPage();
             page.navigate("https://playwright.dev");
             System.out.println(page.title());
